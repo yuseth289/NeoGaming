dfadd4d8c33c738cb9d2f7d279299f4caacc51f1
Yuseth <yuseth28124@gmail.com>
2026-04-24 11:31:30 -0500

    raiway3

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'backend-neogamin-production.up.railway.appapi',
+  apiBaseUrl: 'backend-neogamin-production.up.railway.app/api',
   useMockApi: false
 };
